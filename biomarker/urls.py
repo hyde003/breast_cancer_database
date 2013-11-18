e1246a3fb5ed1a31778ba5d490ae080a19ad96f4
ssridharan <whimsically_divaesque@yahoo.com>
2013-11-18 13:06:40 -0800

    fixed error

--- biomarker/urls.py
+++ biomarker/urls.py
@@ -5,15 +5,11 @@ from report import urls
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
 urlpatterns = patterns('',
-<<<<<<< HEAD
 	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
 	url(r'^index/', include('report.urls')),
 	url(r'^register/', include('registrations.urls')),
-=======
-	url(r'^breastcancer_database/', include('report.urls')),
->>>>>>> 28226f3b0bb6a0d7d2f7e47decf6a0a0140a064c
+
     # Examples:
     # url(r'^$', 'biomarker.views.home', name='home'),
     # url(r'^biomarker/', include('biomarker.foo.urls')),
